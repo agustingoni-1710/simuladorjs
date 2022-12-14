3274e3a1e58c4f1f7d81eee1c1235c1a0c214d39
Agustin Goni <agustingoni1710@gmail.com>
2022-12-14 00:44:22 -0300

    Segunda Entrega

--- main.js
+++ main.js
@@ -103,31 +103,7 @@ switch (opcion) {
         alert("Opción incorrecta, volve a intentar!!!");
         break;
 }
-/* let nombreCliente = prompt("Ingrese su nombre :");
-let monto = prompt("Ingrese el Monto del Plazo Fijo: ");
- */
 
-/* function plazoFijo(monto){
-    if(monto>=1500 && monto<10000000){
-        let interesAnual = monto * 0.75; 
-        return interesAnual;
-        
-    }else if(monto>=10000000){
-        let interesAnual = monto *0.66;
-        return interesAnual;
-        
-    }else{
-        alert("No puedes realizar el plazo fijo : ");
-    }
-       
-} */
-
-
-    /* if(monto>=1500  && monto<10000000){
-        alert (nombreCliente+" tu plazo fijo generara un interes anual de "+(plazoFijo(monto).toFixed(2))+" pesos");
-    }else{
-        alert (nombreCliente+" tu plazo fijo generara un interes anual de "+(plazoFijo(monto).toFixed(2))+" pesos"); 
-    }  */
 
 
  
